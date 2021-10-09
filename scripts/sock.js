b31f8a41f0ae66d70612c1db0d6f4ff81bc1b7c1
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-10-09 19:49:41 +0700

    remove verbose comment

--- scripts/sock.js
+++ scripts/sock.js
@@ -1,12 +1,11 @@
 function calculatePairs(inputs) {
   let lastInput = 0;
   let count = 0;
-  // sort array asc
+
   inputs.sort(function (a, b) {
     return a - b;
   });
 
-  // do main operation
   inputs.forEach((input) => {
     if (lastInput == input) {
       count++;
